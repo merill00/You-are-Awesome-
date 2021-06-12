c1dbd84ce0e43cc3083dbd083bec519906213371
Lloyd Merithew <lloyd.merithew@knowledge2share.com>
2021-06-12 16:56:17 -0400

    Ch. 2.7 Looking Good: App Layout, Auto Resizing, AutoLayout & Constraints Swift Book v4

--- You are Awesome!/ViewController.swift
+++ You are Awesome!/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        // print("👍🏼 viewDidLoad has run!")
-      //  messageLabel.text = "Fabulous? That's You!"
+        messageLabel.text = ""
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
